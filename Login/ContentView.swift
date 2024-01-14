cb8af02179ca99b7f7a5983cca99deee49064211
ABDELHADI BAKHTY <abdelhadibakhty@ABDELHADIs-MacBook-Pro.local>
2024-01-14 06:08:58 +0100

    initial commit

---
+++ Login/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Login
+//
+//  Created by ABDELHADI BAKHTY on 14/1/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State var username = ""
+    @State var password = ""
+    
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
